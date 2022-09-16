48f5e76dede3358fef062940483916236691e8d7
murilokuerten92 <murilokuerten92@gmail.com>
2022-09-16 20:06:32 -0300

    feat: finished coffees array

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 export const Container = styled.main`
    padding: 0 10rem;
 
+   @media (max-width: 1360px) {
+      padding: 0 4rem;
+ }
+
    @media (max-width: 960px) {
       padding: 0 2rem;
  }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,18 +2,17 @@ import styled from 'styled-components'
 
 export const Container = styled.main`
 
-
  .CoffeeTitle {
   margin-top: 4rem;
  } 
 
 `;
 
-
 export const SectionCards = styled.section`
   display: flex;
   flex-wrap: wrap;
   column-gap: 2rem;
+  row-gap: 3rem;
   margin-top: 3rem;
 `;
 
