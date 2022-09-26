76cff44e3b73fdc50aa5c77cec870032fd1154e3
murilokuerten92 <murilokuerten92@gmail.com>
2022-09-26 10:10:58 -0300

    feat: creating checkout screen

--- src/pages/Checkout/Resume/index.tsx
+++ src/pages/Checkout/Resume/index.tsx
@@ -7,10 +7,21 @@ export function Resume() {
     return (
         <S.Container>
             <S.Address>
-
+                <S.Title>Complete seu pedido</S.Title>
+                <S.AddressContent>
+                    <S.TitleAddress>
+                        Endereço de Entrega
+                    </S.TitleAddress>
+                    <S.ResumeInfo>
+                        Informe o endereço onde deseja receber seu pedido
+                    </S.ResumeInfo>
+                </S.AddressContent>
             </S.Address>
             <S.Resume>
+                <S.Title>Cafés selecionados</S.Title>
+                <S.ResumeContent>
 
+                </S.ResumeContent>
             </S.Resume>
         </S.Container>
     )
--- src/pages/Checkout/Resume/styles.ts
+++ src/pages/Checkout/Resume/styles.ts
@@ -3,13 +3,44 @@ import styled from 'styled-components'
 export const Container = styled.main`
   display:grid;
   grid-template-columns: 1fr 448px;
-  background-color: ${({ theme }) => theme.base_card};
+  column-gap: 2rem;
 `;
 
 export const Address = styled.section`
+
+`;
+
+export const AddressContent = styled.div`
   background-color: ${({ theme }) => theme.base_card};
+  padding: 40px;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.text_accent};
+  font-size: 1.2rem;
+  font-family: 'Baloo 2';
+  font-weight: bold;
+`;
+
+export const TitleAddress = styled.h2`
+  color: ${({ theme }) => theme.text_accent};
+  font-size: 1rem;
+  font-family: 'Roboto';
+  font-weight: 400;
+`;
+
+export const ResumeInfo = styled.p`
+  color: ${({ theme }) => theme.text_accent};
+  font-size: 0.8rem;
+  font-family: 'Roboto';
+  font-weight: 400;
 `;
 
 export const Resume = styled.section`
+
+`;
+
+export const ResumeContent = styled.div`
   background-color: ${({ theme }) => theme.base_card};
+  padding: 40px;
 `;
